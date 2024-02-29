376677b2adde780739a7f161c840aeb37bfb41f7
RayyanAlkasaji <ryyanksg@gmail.com>
2024-02-29 21:12:32 +0300

    Add GitHub Actions workflow

--- .github/Atypon Streaming System/authentication-service/src/main/java/com/example/authenticationservice/appuser/AppUserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.authenticationservice.appuser;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppUserRepo extends JpaRepository<AppUser,Long> {
-    AppUser findAppUserByUsername(String username);
-
-
-}
